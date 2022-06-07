816bc4602c7bff212ba38fd5203fa4e7fdd911f8
spumer <spumer-tm@yandex.ru>
2022-06-07 13:04:24 +0500

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,38 @@
+import logging
 import os
 import pathlib
 
 import playhouse.db_url
 import pytest
 import peewee as pw
+
+
+class PeeweeLogUnwrapper(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        if 'extra' not in kwargs:
+            kwargs['extra'] = {}
+
+        if isinstance(msg, tuple):
+            msg, params = msg
+            kwargs['extra']['params'] = params
+
+        return msg, kwargs
+
+
+pw.logger = PeeweeLogUnwrapper(pw.logger, {})
+
+
+@pytest.fixture()
+def assert_log_has_records(caplog):
+    caplog.set_level(logging.DEBUG, logger='peewee')
+
+    def _inner(*messages, level=logging.DEBUG):
+        records = [r for r in caplog.records if r.levelno >= level]
+        recorded_messages = [r.message for r in records]
+        for msg in messages:
+            assert msg in recorded_messages
+
+    return _inner
 
 
 @pytest.fixture()
--- tests/test_migrator.py
+++ tests/test_migrator.py
@@ -1,6 +1,16 @@
+import logging
+import typing
+from unittest.mock import ANY
+
+import peewee as pw
+from typing import NamedTuple
+from typing import Type
+
 from unittest import mock
 
 import pytest
+
+from peewee_migrate import Migrator
 
 
 @pytest.fixture()
@@ -13,98 +23,280 @@ def _mock_connection():
         yield
 
 
-def test_migrator():
-    import peewee as pw
+def model_has_index(model: Type[pw.Model], *, name, fields=ANY):
+    meta: pw.Metadata = model._meta
+
+    repr_indexes = [{
+        'name': idx._name,
+        'fields': list(idx._expressions),
+    } for idx in meta.fields_to_index()]
+
+    if fields is not ANY:
+        # repr_indexes['fields'] is tuple
+        fields = list(fields)
+
+    return {
+        'name': name,
+        'fields': fields,
+    } in repr_indexes
+
+
+@pytest.fixture()
+def sqlite_migrator():
     from playhouse.db_url import connect
-    from peewee_migrate import Migrator
 
     database = connect('sqlite:///:memory:')
-    migrator = Migrator(database)
+    return Migrator(database)
+
+
+@pytest.fixture()
+def migrator(sqlite_migrator):
+    migrator = sqlite_migrator
 
     @migrator.create_table
     class Customer(pw.Model):
         name = pw.CharField()
 
-    assert Customer == migrator.orm['customer']
-
     @migrator.create_table
     class Order(pw.Model):
         number = pw.CharField()
         uid = pw.CharField(unique=True)
-
         customer_id = pw.ForeignKeyField(Customer, column_name='customer_id')
 
-    assert Order == migrator.orm['order']
     migrator.run()
 
-    migrator.add_columns(Order, finished=pw.BooleanField(default=False))
+    yield migrator
+
+    assert not migrator.ops, "migrator.run() or migrator.clean() call missing"
+
+
+@pytest.mark.parametrize('alias', [False, True])
+def test_add_columns(migrator, alias):
+    """add_columns reflect changes in model meta"""
+    Order = migrator.orm['order']
+    if not alias:
+        migrator.add_columns(Order, finished=pw.BooleanField(default=False))
+    else:
+        migrator.add_fields(Order, finished=pw.BooleanField(default=False))
     assert 'finished' in Order._meta.fields
     migrator.run()
 
-    migrator.drop_columns('order', 'finished', 'customer_id', 'uid')
-    assert 'finished' not in Order._meta.fields
+
+@pytest.mark.parametrize('alias', [False, True])
+def test_drop_columns(migrator, alias):
+    """
+    drop_columns change both: model meta and instance
+    and ignore unknown fields
+    """
+
+    Order = migrator.orm['order']
+    customer_id_object_id_name = Order.customer_id.object_id_name
+
+    assert 'uid' in Order._meta.fields  # common field
+    assert hasattr(Order, customer_id_object_id_name)  # ForeignKey
+
+    migrator.drop_columns(Order, 'uid', 'customer_id')
+
+    assert 'uid' not in Order._meta.fields
     assert not hasattr(Order, 'customer_id')
-    assert not hasattr(Order, 'customer_id_id')
+    assert not hasattr(Order, customer_id_object_id_name)
+
     migrator.run()
 
-    migrator.add_columns(Order, customer=pw.ForeignKeyField(Customer, null=True))
-    assert 'customer' in Order._meta.fields
-    assert Order.customer.name == 'customer'
-    migrator.run()
-    assert Order.customer.name == 'customer'
 
-    migrator.rename_column(Order, 'number', 'identifier')
+def test_add_nullable_foreign_key(migrator):
+    Order = migrator.orm['order']
+    Customer = migrator.orm['customer']
+
+    migrator.add_columns(Order, nullable_customer=pw.ForeignKeyField(Customer, null=True))
+    assert 'nullable_customer' in Order._meta.fields
+    assert Order.nullable_customer.name == 'nullable_customer'
+    assert hasattr(Order, 'nullable_customer_id')  # check object_id_name
+
+    migrator.run()
+
+
+@pytest.mark.parametrize('alias', [False, True])
+def test_rename_column(migrator, alias):
+    Order = migrator.orm['order']
+
+    if not alias:
+        migrator.rename_column(Order, 'number', 'identifier')
+    else:
+        migrator.rename_field(Order, 'number', 'identifier')
     assert 'identifier' in Order._meta.fields
+
     migrator.run()
 
-    migrator.drop_not_null(Order, 'identifier')
-    assert Order._meta.fields['identifier'].null
-    assert Order._meta.columns['identifier'].null
-    migrator.run()
-
-    migrator.add_default(Order, 'identifier', 11)
-    assert Order._meta.fields['identifier'].default == 11
-    migrator.run()
-
-    migrator.change_columns(Order, identifier=pw.IntegerField(default=0))
-    assert Order.identifier.field_type == 'INT'
-    migrator.run()
-
-    Order.create(identifier=55)
-    migrator.sql('UPDATE "order" SET identifier = 77;')
-    migrator.run()
-    order = Order.get()
-    assert order.identifier == 77
-
-    migrator.add_index(Order, 'identifier', 'customer')
-    migrator.run()
-    assert Order._meta.indexes
-    assert not Order.identifier.index
-
-    migrator.drop_index(Order, 'identifier', 'customer')
-    migrator.run()
-    assert not Order._meta.indexes
-
-    migrator.remove_fields(Order, 'customer')
-    migrator.run()
-    assert not hasattr(Order, 'customer')
-
-    migrator.add_index(Order, 'identifier', unique=True)
-    migrator.run()
-    assert not Order.identifier.index
-    assert Order.identifier.unique
-    assert Order._meta.indexes
-
-    # TODO fix change_columns
-    # migrator.change_columns(Order, identifier=pw.IntegerField(default=0))
-    # assert not Order._meta.indexes
-    # migrator.run()
+def test_rename_columne(migrator):
+    Order = migrator.orm['order']
 
     migrator.rename_table("order", "new_name")
     migrator.run()
     assert Order._meta.table_name == "new_name"
     migrator.rename_table("new_name", "order")
     migrator.run()
+
+
+def test_drop_not_null(migrator):
+    Order = migrator.orm['order']
+
+    assert not Order._meta.fields['number'].null
+    migrator.drop_not_null(Order, 'number')
+    assert Order._meta.fields['number'].null
+    assert Order._meta.columns['number'].null
+
+    migrator.run()
+
+
+def test_add_default(migrator):
+    Order = migrator.orm['order']
+
+    migrator.add_default(Order, 'number', '11')
+    assert Order._meta.fields['number'].default == '11'
+
+    migrator.run()
+
+
+@pytest.mark.parametrize('alias', [False, True])
+def test_change_columns_change_field_type(migrator, alias):
+    Order = migrator.orm['order']
+
+    assert Order.number.field_type != 'INT'
+    if not alias:
+        migrator.change_columns(Order, number=pw.IntegerField(default=0))
+    else:
+        migrator.change_fields(Order, number=pw.IntegerField(default=0))
+    assert Order.number.field_type == 'INT'
+    assert Order._meta.fields['number'].default == 0
+
+    migrator.run()
+
+
+def test_raw_sql_migration(migrator):
+    Customer = migrator.orm['customer']
+
+    Customer.create(name='test')
+    migrator.sql("UPDATE customer SET name = 'not-test';")
+    migrator.run()
+
+    order = Customer.get()
+    assert order.name == 'not-test'
+
+
+
+"""
+    @migrator.create_table
+    class Test(pw.Model):
+        indexed_not_unique = pw.CharField(index=True)
+"""
+
+
+def test_add_and_remove_index(migrator, assert_log_has_records):
+    @migrator.create_table
+    class Test(pw.Model):
+        one = pw.CharField(column_name='not_one')
+        two = pw.IntegerField()
+
+        three = pw.CharField()
+        four = pw.CharField()
+
+        class Meta:
+            indexes = [
+                # common index
+                (('three', 'four'), False),
+                # index with custom column name
+                (('one', 'three'), False),
+            ]
+
+    migrator.run()  # FIXME: LazyOperation: add_index reflect meta and create_table create indexes from Meta, then add_index fails
+
+    # by default indexes created safely with 'IF NOT EXISTS'
+    assert_log_has_records(
+        'CREATE INDEX IF NOT EXISTS "test_three_four" ON "test" ("three", "four")',
+        'CREATE INDEX IF NOT EXISTS "test_not_one_three" ON "test" ("not_one", "three")',
+    )
+
+    assert model_has_index(Test, name='test_three_four', fields=[Test.three, Test.four])
+    assert model_has_index(Test, name='test_not_one_three', fields=[Test.one, Test.three])
+
+    # manually add common and composite indexes ...
+    migrator.add_index(Test, 'one')
+    migrator.add_index(Test, 'one', 'two')
+    migrator.run()
+
+    assert_log_has_records(
+        'CREATE INDEX "test_not_one" ON "test" ("not_one")',
+        'CREATE INDEX "test_not_one_two" ON "test" ("not_one", "two")',
+    )
+
+    assert model_has_index(Test, name='test_not_one', fields=[Test.one])
+    assert model_has_index(Test, name='test_not_one_two', fields=[Test.one, Test.two])
+
+    # ... drop manually added common and composite indexes
+    migrator.drop_index(Test, 'one')
+    migrator.drop_index(Test, 'one', 'two')
+    migrator.run()
+
+    assert_log_has_records(
+        'DROP INDEX "test_not_one"',
+        'DROP INDEX "test_not_one_two"',
+    )
+
+    assert not model_has_index(Test, name='test_not_one', fields=[Test.one])
+    assert not model_has_index(Test, name='test_not_one_two', fields=[Test.one, Test.two])
+
+    # ensure pre-defined indexes can be dropped too
+    migrator.drop_index(Test, 'three', 'four')
+    migrator.drop_index(Test, 'one', 'three')
+    migrator.run()
+    assert_log_has_records(
+        'DROP INDEX "test_three_four"',
+        'DROP INDEX "test_not_one_three"',
+    )
+
+    assert not model_has_index(Test, name='test_three_four', fields=[Test.three, Test.four])
+    assert not model_has_index(Test, name='test_not_one_three', fields=[Test.one, Test.three])
+
+
+def test_add_index_unique_single(migrator):
+    Order = migrator.orm['order']
+
+    migrator.add_index(Order, 'number', unique=True)
+    migrator.run()
+
+    assert not Order.number.index
+    assert Order.number.unique  # FIXME: why we make field instance unique?
+    assert Order._meta.indexes == [(('number',), True)]
+
+
+def test_add_index_unique_multiple(migrator):
+    Order = migrator.orm['order']
+
+    migrator.add_fields(Order, new_number=pw.CharField(null=True))
+    migrator.add_index(Order, 'number', 'new_number', unique=True)
+    migrator.run()
+
+    assert not Order.number.index
+    assert not Order.new_number.index
+    assert not Order.new_number.unique
+    assert not Order.new_number.unique
+    assert Order._meta.indexes == [(('number', 'new_number'), True)]
+
+
+def test_change_columns_drop_field_defined_unique_index(migrator):
+    """Unique index can be defined only on field level when metadata was empty
+
+    Check we can correctly drop this index when change column
+    """
+    Order = migrator.orm['order']
+
+    # edge-case: Metadata unfilled in original model
+    assert not Order._meta.indexes
+    assert Order.uid.unique
+    migrator.change_columns(Order, uid=pw.IntegerField(default=0))
+    migrator.run()
+    assert not Order._meta.indexes
 
 
 def test_migrator_postgres(_mock_connection):
